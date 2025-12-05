3f35c2c8e74b6cf698e0d0afd7cd6b94521a0a07
Shankar Nidhish P <shankarnidhish24@gmail.com>
2025-12-05 20:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (40.95%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -14,22 +14,13 @@
  * }
  */
 class Solution {
-    Stack<TreeNode> st1 = new Stack<>();
-      Stack<TreeNode> st2 = new Stack<>();
     public TreeNode invertTree(TreeNode root) {
-        
-
          return invert(root);
     }
     public TreeNode invert(TreeNode root){
       if(root==null){
         return root;
       }
-      st1.add(root.left);
-      
-      st1.add(root.right);
-    
-
    TreeNode left =  invert(root.left);
    TreeNode right = invert(root.right);
 
