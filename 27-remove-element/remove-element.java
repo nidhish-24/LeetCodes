af458a30b6048a650c806f57e4886b0364c440f3
Shankar Nidhish P <shankarnidhish24@gmail.com>
2026-02-06 12:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (75.84%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int k=nums.length;
+        int i=0;
+
+        while(i<k){
+            if(nums[i]==val){
+                nums[i]=nums[k-1];
+                k--;
+        }
+            else{
+            i++;
+        }
+
+        }
+
+        return k;
+    }
+}
