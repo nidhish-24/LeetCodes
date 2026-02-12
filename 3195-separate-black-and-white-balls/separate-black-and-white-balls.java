f8038ec7b77c9c2f6ecf41a2373026720c5fef2c
Shankar Nidhish P <shankarnidhish24@gmail.com>
2026-02-12 10:09:12 +0530

    Time: 7 ms (99.26%) | Memory: 47.7 MB (50.30%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,13 +1,12 @@
 class Solution {
     public long minimumSteps(String s) {
-        long black = 0;
-        long steps = 0;
-
-        for (char c : s.toCharArray()) {
-            if (c == '1') {
-                black++;      // count black balls
-            } else {          // c == '0', white ball
-                steps += black;  // this white ball must move past all black balls
+       long black=0;
+       long steps=0;
+        for(char c:s.toCharArray()){
+            if(c=='1'){
+                black++;
+            }else{
+                steps+=black;
             }
         }
         return steps;
