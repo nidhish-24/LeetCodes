e89d5496f89cfad9e117aa9f40be9df7c0436058
Shankar Nidhish P <shankarnidhish24@gmail.com>
2026-03-03 13:46:36 +0530

    Time: 16 ms (59.40%) | Memory: 47.2 MB (55.46%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -4,17 +4,20 @@ class Solution {
         int left=0;
         int right=0;
         int start=0;
-
-        HashMap<Character,Integer> map = new HashMap<>();
-
-        for(int i=0;i<t.length();i++){
-          map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
-        }
-        int required = t.length();
         int minlen=Integer.MAX_VALUE;
         
+        if (s.length() == 0 || t.length() == 0) return "";
+        HashMap<Character , Integer> map=new HashMap<>();
+
+        for(int i=0;i<t.length();i++){
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
+        }
+        
+        int required = t.length();
+
         while(right<s.length()){
-            char r=s.charAt(right);
+        
+        char r = s.charAt(right);
             if(map.containsKey(r)){
                 if(map.get(r)>0){
                     required--;
@@ -22,28 +25,26 @@ class Solution {
                 map.put(r,map.get(r)-1);
             }
 
-            while(required==0){
+        while(required==0){
 
-                if(right-left+1<minlen){
-                    minlen=right-left+1;
-                    start=left;
-                }
-
-                char l=s.charAt(left);
-
-                if(map.containsKey(l)){
-                    map.put(l,map.get(l)+1);
-                    if(map.get(l)>0){
-                        required++;
-                    }
-                }
-                left++;
+            if(right-left+1<minlen){
+                minlen=right-left+1;
+                start=left;
             }
-            right++;
+
+            char l = s.charAt(left);
+
+            if(map.containsKey(l)){
+                map.put(l,map.get(l)+1);
+                if(map.get(l)>0){
+                    required++;
+                }
+            }
+            left++;
+        }
+        right++;
         }
 
-        return  (minlen==Integer.MAX_VALUE) ? "" : s.substring(start,minlen+start);
-
-
+        return (minlen==Integer.MAX_VALUE)?"":s.substring(start,minlen+start);
     }
 }
