579db7b0ed1bea5d14fee914722eaf646f62b786
Shankar Nidhish P <shankarnidhish24@gmail.com>
2026-03-09 12:08:43 +0530

    Time: 1 ms (43.80%) | Memory: 43.8 MB (36.31%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean findRotation(int[][] matrix, int[][] target) {
+        
+         
+        for(int i=0;i<4;i++){
+           
+           boolean res = Arrays.deepEquals(matrix,target);
+           
+           if(res==true){
+            return true;
+           }
+
+           matrixrotation(matrix);
+
+        }
+        return false;
+
+    }
+        void matrixrotation(int[][] matrix){
+           int n=matrix.length;
+           for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+           }
+
+
+           for(int i=0;i<n;i++){
+              int left=0;
+              int right=n-1;
+
+              while(left<right){
+                   int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+
+                left++;
+                right--;
+              }
+
+           }
+    }
+}
