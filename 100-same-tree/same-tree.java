1e56e4a54600ef439d4b477a793869dbadfc8e72
Shankar Nidhish P <shankarnidhish24@gmail.com>
2026-03-13 15:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (68.60%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,17 +14,27 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode root1, TreeNode root2) {
-        
-        if(root1==null && root2==null){
-            return true;
-        }
-        if(root1==null || root2==null || root1.val!=root2.val ){
-            return false;
-        }
-       
-
-        return isSameTree(root1.left,root2.left) && isSameTree(root1.right,root2.right);
-
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+      if(p==null && q==null){
+        return true;
+      }
+      if(p==null || q==null){
+        return false;
+      }
+      return sameTree(p,q);
     }
-}
+
+    public boolean sameTree(TreeNode p, TreeNode q){
+        if(p==null && q==null){
+        return true;
+      }
+      if(p==null || q==null){
+        return false;
+      }
+      if(p.val!=q.val){
+        return false;
+      }
+      
+      return sameTree(p.left,q.left) && sameTree(p.right,q.right);
+    } 
+    } 
